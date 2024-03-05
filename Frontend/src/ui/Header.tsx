9564eb8ddbb4042ce73256438e75ce6c1c1af4e9
Khai Xuen Tang <angryxuen@gmail.com>
2024-03-05 01:44:39 +0000

    Added new file directory, write Header and UserInfoButton

---
+++ Frontend/src/ui/Header.tsx
@@ -0,0 +1,12 @@
+import UserInfoButton from './component/UserInfoButton'
+
+const Header = () => {
+    return (
+        <header>
+            <h2 className="font-bold">Seamless-Scheduler</h2>
+            <UserInfoButton />
+        </header>
+    )
+}
+
+export default Header
---
+++ Frontend/src/ui/component/UserInfoButton.tsx
@@ -0,0 +1,12 @@
+const UserInfoButon = () => {
+    return (
+        <button>
+            <div>
+                <p>name</p>
+            </div>
+        </button>
+    )
+}
+
+export default UserInfoButon
+
